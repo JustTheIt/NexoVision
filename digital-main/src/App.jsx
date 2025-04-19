c2e2f248832d69f1c6e7e1d21a8305e9224524c6
Bishwa Sharma <bishwarijal07@gmail.com>
2025-04-19 14:29:25 +0545

    push

---
+++ digital-main/src/App.jsx
@@ -0,0 +1,34 @@
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import AboutSection from './components/AboutSection';
+import WhyUs from './components/WhyUs';
+import Services from './components/Services';
+import MarketingTools from './components/MarketingTools';
+import Testimonials from './components/Testimonials';
+import Team from './components/Team';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import AdvertisingCalculator from './components/AdvertisingCalculator';
+
+
+function App() {
+  const data = null; 
+  return (
+    <div className="font-sans">
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <WhyUs />
+      <Services />
+      <MarketingTools />
+      <Testimonials />
+      <Team />   
+      <AdvertisingCalculator />
+      <ContactUs /> 
+      <Footer />
+     
+    </div>
+  );
+}
+
+export default App;
